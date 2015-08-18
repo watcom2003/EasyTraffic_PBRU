f67468abed8b468007c585a13b605089fe23ed03
watcom2003 <watcom@hotmail.com>
2015-08-18 11:26:31 +0700

    before break

---
+++ app/src/main/java/th/ac/pbru/suwat/easytraffic/MyAdapter.java
@@ -0,0 +1,47 @@
+package th.ac.pbru.suwat.easytraffic;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by John on 18/8/2558.
+ */
+public class MyAdapter extends BaseAdapter {
+    // Explicit
+    private Context objContext;
+    private int[] iconInts;
+    private String[] titleStrings;
+
+    public MyAdapter(Context objContext, int[] iconInts, String[] titleStrings) {
+        this.objContext = objContext;
+        this.iconInts = iconInts;
+        this.titleStrings = titleStrings;
+    } // Constructor
+
+
+    @Override
+    public int getCount() {
+        return titleStrings.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater objLayoutInflater = (LayoutInflater) 
+
+
+        return null;
+    }
+}  // Main Class
