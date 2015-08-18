64ffc3cc7333243ca8be2de219e9893f9d4b9d69
watcom2003 <watcom@hotmail.com>
2015-08-18 13:51:53 +0700

    after_lunch

--- app/src/main/java/th/ac/pbru/suwat/easytraffic/MainActivity.java
+++ app/src/main/java/th/ac/pbru/suwat/easytraffic/MainActivity.java
@@ -20,11 +20,49 @@ public class MainActivity extends Activity {
         // Initial Widget กด ALT + Enter
         initialWidget();
 
+        // Create ListView
+        createListView();
+
     }  // Main Method นี่คือ เมธอดหลัก
+
+    private void createListView() {
+        String[] strTitle = new String[20];
+        strTitle[0] = "ห้ามเลี้ยวซ้าย";
+        strTitle[1] = "ห้ามเลี้ยวขวา";
+        strTitle[2] = "ตรงไป";
+        strTitle[3] = "เลี้ยวขวา";
+        strTitle[4] = "เลี้ยวซ้าย";
+        strTitle[5] = "ออก";
+        strTitle[6] = "เข้า";
+        strTitle[7] = "ออก";
+        strTitle[8] = "หยุด";
+        strTitle[9] = "จำกัดความสูง";
+        strTitle[10] = "ทางแยก";
+        strTitle[11] = "ห้ามกลับรถ";
+        strTitle[12] = "ห้ามจอด";
+        strTitle[13] = "รถสวน";
+        strTitle[14] = "ห้ามแซง";
+        strTitle[15] = "เข้า";
+        strTitle[16] = "หยุดตรวจ";
+        strTitle[17] = "จำกัดความเร็ว";
+        strTitle[18] = "จำกัดความกว้าง";
+        strTitle[19] = "จำกัดความสูง";
+
+        int[] intImage = {R.drawable.traffic_01, R.drawable.traffic_02, R.drawable.traffic_03,
+                R.drawable.traffic_04, R.drawable.traffic_05, R.drawable.traffic_06,
+                R.drawable.traffic_07, R.drawable.traffic_08, R.drawable.traffic_09,
+                R.drawable.traffic_10, R.drawable.traffic_11, R.drawable.traffic_12,
+                R.drawable.traffic_13, R.drawable.traffic_14, R.drawable.traffic_15,
+                R.drawable.traffic_16, R.drawable.traffic_17, R.drawable.traffic_18,
+                R.drawable.traffic_19, R.drawable.traffic_20};
+
+        MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, intImage, strTitle);
+        trafficListView.setAdapter(objMyAdapter);
+
+     }
 
     private void initialWidget() {
         trafficListView = (ListView) findViewById(R.id.listView);
-
     }
 
 
--- app/src/main/java/th/ac/pbru/suwat/easytraffic/MyAdapter.java
+++ app/src/main/java/th/ac/pbru/suwat/easytraffic/MyAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by John on 18/8/2558.
@@ -39,9 +41,19 @@ public class MyAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater objLayoutInflater = (LayoutInflater) 
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view1 = objLayoutInflater.inflate(R.layout.listview_traffic, parent, false);
+
+        // Show image
+        ImageView iconImageView = (ImageView) view1.findViewById(R.id.imvicon);
+        iconImageView.setImageResource(iconInts[position]);
+
+        // Show Title
+        TextView titleTextView = (TextView) view1.findViewById(R.id.txtShowTitle);
+        titleTextView.setText(titleStrings[position]);
 
 
-        return null;
-    }
+        return view1;
+    }  // get view
 }  // Main Class
